fded27705a4890a56325db8cf326c17632c9c9a6
HristoFanev <107874286+HristoFanev@users.noreply.github.com>
2023-01-12 11:12:32 +0200

    Zadacha

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("elementi na opashkata ");
+            Queue<int> queue = new Queue<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                queue.Enqueue(int.Parse(Console.ReadLine()));
+            }
+            Console.WriteLine("poledniq -"+ queue.Peek());
+            Console.WriteLine("minimalniq -"+ queue.Min());
+            
+        }
+    }
+}
---
+++ zadaca/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace zadaca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            for (int i = 1; i < N; i += 3) 
+            {
+                Console.WriteLine(i);
+            }  
+        }
+    }
+}
